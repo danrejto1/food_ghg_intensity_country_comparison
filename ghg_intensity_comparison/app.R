807da107cd72eb2dbdb301c4da1f31a55a8d0f31
Dan Rejto <danrejto@gmail.com>
2019-05-19 09:54:46 -0700

    switched from ggplot to plotly and added caption

--- ghg_intensity_comparison/app.R
+++ ghg_intensity_comparison/app.R
@@ -9,10 +9,11 @@
 
 library(shiny)
 library(plotly)
+library(readr)
 library(dplyr)
 library(ggplot2)
 
-df <- read.csv( file = "emission_intensities.csv")
+df <- read_csv( file = "emission_intensities.csv")
 items <- unique(df$Item)
 countries <- unique(df$Area)
 
@@ -44,17 +45,33 @@ ui <- fluidPage(
 
 # Define server logic required to draw plot
 server <- function(input, output) {
-
-  
+   
    output$plot <- renderPlotly({
-      df %>% 
-         filter(Area %in% input$countries,
-                Item == input$item) %>%   
-         ggplot() +
-         geom_col(aes(x = Area, y=Value), fill = "#0D4459")+
-         labs(x="", y="kg CO2eq/kg product",
-              title = paste0("Comparison of Greenhouse Gas Emissions for ", input$item))+
-         theme_minimal() 
+      pd <- filter(df, Area %in% input$countries,
+                   Item == input$item)
+      
+      plot_ly(data = pd, x=~Area, y=~Value, type = "bar", color=I("#0D4459"))  %>% 
+         layout(title = paste0("Comparison of Greenhouse Gas Emissions for \n", input$item),
+                xaxis = list(
+                   title = ""),
+                yaxis = list(
+                   title = "kg CO2eq/kg product"),
+                annotations = 
+                   list(x = -.05, y = 0, text = "@DanRejto \nData: FAOSTAT", 
+                        showarrow = F, xref='paper', yref='paper', 
+                        xanchor='right', yanchor='auto', xshift=0, yshift=0, textangle=-90,
+                        font=list(size=8, color="black"))
+                )
+      
+            # df %>% 
+      #    filter(Area %in% input$countries,
+      #           Item == input$item) %>%   
+      #    ggplot() +
+      #    geom_col(aes(x = Area, y=Value), fill = "#0D4459")+
+      #    labs(x="", y="kg CO2eq/kg product",
+      #         title = paste0("Comparison of Greenhouse Gas Emissions for ", input$item),
+      #         caption = "Created by @DanRejto \nData: FAOSTAT")+
+      #    theme_minimal() 
 
    })
 }
